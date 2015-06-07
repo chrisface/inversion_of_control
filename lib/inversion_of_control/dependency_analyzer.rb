a379562130a2588ee93b7c950f74b11393a16215
chrisface <chrisface@gmail.com>
2015-06-07 23:08:08 +0100

    Add the ability to create dependency graphs

--- lib/inversion_of_control/dependency_analyzer.rb
+++ lib/inversion_of_control/dependency_analyzer.rb
@@ -1,7 +1,30 @@
+require 'graphviz'
 module InversionOfControl
-  class DependencyAnalyzer
 
-    attr_accessor :tracked_classes, :dependency_trees
+  class Node
+    attr_accessor :edges, :resolved_dependency, :dependency_name
+
+    def initialize(resolved_dependency, dependency_name)
+      @resolved_dependency = resolved_dependency
+      @dependency_name = dependency_name
+      @edges = []
+    end
+
+    def add_edge(edges)
+      @edges << edges
+    end
+
+    def name
+      resolved_dependency.to_s
+    end
+
+    def type
+      resolved_dependency.class
+    end
+  end
+
+  class DependencyAnalyzer
+    attr_accessor :tracked_classes
 
     def initialize
       @tracked_classes = []
@@ -9,6 +32,69 @@ module InversionOfControl
 
     def track_class(klass)
       @tracked_classes << klass
+    end
+
+    def generate_graph(file_path)
+      nodes = {}
+
+      @tracked_classes.each do |klass|
+
+        # Check to see if the dependency has already been found
+        node = nodes[klass]
+
+        unless node
+          # The name of a dependency can only be resolved by a dependant
+          node = Node.new(klass, "unknown")
+          nodes[klass] = node
+        end
+
+        klass.resolve_dependencies_from_class.each do |dependency, resolved_dependency|
+
+          # The dependency may have incorrectly been assumed to be a static Class
+          # dependency based on the order of discovery
+          if child_node = nodes[resolved_dependency]
+
+            # If the dependency was already registered and had no name, set the name
+            # now that we have discovered it via a dependency
+            if child_node.dependency_name == "unknown"
+              child_node.dependency_name = dependency
+            end
+          elsif child_node = nodes[resolved_dependency.class]
+            # The dependency was mistaken for a static class dependency
+            child_node.resolved_dependency = resolved_dependency
+
+            nodes.delete(resolved_dependency.class)
+            nodes[resolved_dependency] = child_node
+
+            if child_node.dependency_name == "unknown"
+              child_node.dependency_name = dependency
+            end
+          else
+            child_node = Node.new(resolved_dependency, dependency)
+            nodes[resolved_dependency] = child_node
+          end
+
+          node.add_edge(child_node)
+        end
+      end
+
+      draw_graph(nodes, file_path)
+    end
+
+    def draw_graph(nodes, file_path)
+      g = GraphViz.new(:G, :type => :digraph)
+      drawn_nodes = Hash.new{ |hash, key| hash[key] = g.add_nodes("#{key.dependency_name}\n#{key.type}\n#{key.name}") }
+
+      nodes.values.each do |from_node|
+        gviz_node_from = drawn_nodes[from_node]
+
+        from_node.edges.each do |to_node|
+          gviz_node_to = drawn_nodes[to_node]
+          g.add_edges(gviz_node_from, gviz_node_to)
+        end
+      end
+
+      g.output( :png => file_path )
     end
   end
 end
--- spec/inversion_of_control/dependency_analyzer_spec.rb
+++ spec/inversion_of_control/dependency_analyzer_spec.rb
@@ -11,4 +11,162 @@ describe InversionOfControl::DependencyAnalyzer do
       end
     end
   end
+
+  context "#generate_graph" do
+
+    context "circular dependency" do
+      context "between two classes" do
+        before(:each) do
+          class DependencyA
+            include InversionOfControl
+            inject(:dependency_b)
+          end
+
+          class DependencyB
+            include InversionOfControl
+            inject(:dependency_a)
+          end
+
+          InversionOfControl.configure do |config|
+            config.auto_resolve_unregistered_dependency = true
+          end
+        end
+
+        after(:each) do
+          [:DependencyA, :DependencyB ].each do |class_symbol|
+            Object.send(:remove_const, class_symbol)
+          end
+        end
+
+        it "generates a graph for the cyclical dependency" do
+          InversionOfControl.dependency_analyzer.generate_graph('tmp/cyclic_dependency_2_classes.png')
+        end
+      end
+
+      context "through three classes" do
+        before(:each) do
+          class DependencyA
+            include InversionOfControl
+            inject(:dependency_b)
+          end
+
+          class DependencyB
+            include InversionOfControl
+            inject(:dependency_c)
+          end
+
+          class DependencyC
+            include InversionOfControl
+            inject(:dependency_a)
+          end
+
+          InversionOfControl.configure do |config|
+            config.auto_resolve_unregistered_dependency = true
+          end
+        end
+
+        after(:each) do
+          [:DependencyA, :DependencyB, :DependencyC ].each do |class_symbol|
+            Object.send(:remove_const, class_symbol)
+          end
+        end
+
+        it "generates a graph for the cyclical dependency" do
+          InversionOfControl.dependency_analyzer.generate_graph('tmp/cyclic_dependency_3_classes.png')
+        end
+      end
+
+      context "with a loopback" do
+        before(:each) do
+          class DependencyA
+            include InversionOfControl
+            inject(:dependency_b)
+          end
+
+          class DependencyB
+            include InversionOfControl
+            inject(:dependency_c)
+          end
+
+          class DependencyC
+            include InversionOfControl
+            inject(:dependency_a, :dependency_b, :dependency_d)
+          end
+
+          class DependencyD
+            include InversionOfControl
+            inject(:dependency_b)
+          end
+
+          InversionOfControl.configure do |config|
+            config.auto_resolve_unregistered_dependency = true
+          end
+        end
+
+        after(:each) do
+          [:DependencyA, :DependencyB, :DependencyC, :DependencyD ].each do |class_symbol|
+            Object.send(:remove_const, class_symbol)
+          end
+        end
+
+        it "generates a graph for the cyclical dependency" do
+          InversionOfControl.dependency_analyzer.generate_graph('tmp/cyclic_dependency_loopback.png')
+        end
+      end
+    end
+
+    context "complex tree structure with constructed and static dependencies" do
+      before(:each) do
+        class UserRepository ; end
+        class MailGun
+          include InversionOfControl
+          inject(:mail_api_key)
+        end
+        class OrderRepository ; end
+
+        class UserService
+          include InversionOfControl
+          inject(:user_repository)
+        end
+
+        class OrderManager
+          include InversionOfControl
+          inject(:order_repository, :mail_service, :manager_config)
+        end
+
+        class UserOrders
+          include InversionOfControl
+          inject(:order_manager, :user_service)
+        end
+
+        InversionOfControl.configure do |config|
+          config.auto_resolve_unregistered_dependency = true
+          config.instantiate_dependencies = true
+          config.dependencies = {
+            mail_api_key: "1234",
+            mail_service: {
+              dependency: MailGun,
+              instantiate: false
+            },
+            manager_config: {
+              dependency: {
+                auto_accept: true
+              }
+            }
+          }
+        end
+      end
+
+      after(:each) do
+        test_classes = [:UserRepository, :MailGun, :OrderRepository, :UserService, :OrderManager, :UserOrders]
+        test_classes.each do |class_symbol|
+          Object.send(:remove_const, class_symbol)
+        end
+      end
+
+      it "generates a graph for the complex tree" do
+        InversionOfControl.dependency_analyzer.generate_graph('tmp/complex_tree.png')
+      end
+    end
+  end
 end
